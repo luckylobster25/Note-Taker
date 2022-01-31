f5a639030aab9d883740eba63a0c443055fef9a1
Chue Chang <chuechang1996@outlook.com>
2022-01-31 00:11:35 -0600

    add html route, folder, file and some require

--- server.js
+++ server.js
@@ -1,20 +1,17 @@
 const express = require('express');
-const path = require('path');
+const htmlPath = require('./routes/html');
+const apiPath = require('./routes/index');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
+app.use(htmlPath);
+app.use(apiPath);
 
-app.get('/send', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/sendFile.html'))
-);
-
-app.get('/routes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/routes.html'))
-);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
